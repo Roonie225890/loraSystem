9c3fdd51b615dedc94554d582b2727c86a35f97f
Roonie Hsieh <wei225890@hotmail.com>
2021-12-17 10:44:47 +0800

    Update dbConnect.go

--- database/dbConnect.go
+++ database/dbConnect.go
@@ -4,7 +4,7 @@ import "github.com/jinzhu/gorm"
 
 func DbConnect() (*gorm.DB, error) {
 
-	db, err := gorm.Open("mysql", "root:harbinger@/iot_db?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:pwd@/iot_db?charset=utf8&parseTime=True&loc=Local")
 
 	return db, err
 }
